fde346dcc78aefe084b67af40c41d18c32052026
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-07-27 03:12:36 +0900

    QueryParseExceptionにドキュメンテーションコメントを追加

--- src/com/m12i/query/parser/QueryParseException.java
+++ src/com/m12i/query/parser/QueryParseException.java
@@ -1,5 +1,8 @@
 package com.m12i.query.parser;
 
+/**
+ * クエリ内容に問題があり解析に失敗したことを示す例外.
+ */
 public class QueryParseException extends Exception {
 	private static final long serialVersionUID = 1506417149680232859L;
 	QueryParseException(Throwable cause) {
