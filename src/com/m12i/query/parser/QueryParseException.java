4d96b22ed42ba68e3dbe0ff20cee6cddca8ceedc
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-07-27 03:04:40 +0900

    QueryParseExceptionのコンストラクタのアクセスレベルを変更

--- src/com/m12i/query/parser/QueryParseException.java
+++ src/com/m12i/query/parser/QueryParseException.java
@@ -2,7 +2,7 @@ package com.m12i.query.parser;
 
 public class QueryParseException extends Exception {
 	private static final long serialVersionUID = 1506417149680232859L;
-	public QueryParseException(Throwable cause) {
+	QueryParseException(Throwable cause) {
 		super(cause);
 	}
 }
