a58fb113b9a78fb186e93259544aa907625468fb
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-07-27 03:12:56 +0900

    QueryFactoryにドキュメンテーションコメントを追加

--- src/com/m12i/query/parser/QueryFactory.java
+++ src/com/m12i/query/parser/QueryFactory.java
@@ -2,18 +2,34 @@ package com.m12i.query.parser;
 
 import com.m12i.code.parse.ParseException;
 
+/**
+ * 文字列として表現されたクエリをパースして解析済みクエリを生成するファクトリ・オブジェクト.
+ * @param <E> 解析済みクエリによる検索対象となるコレクションの要素型
+ */
 public final class QueryFactory<E> {
 	private static final ExpressionParser p = new ExpressionParser();
 	private final Accessor<E> a;
+	/**
+	 * ファクトリ・オブジェクトのコンストラクタ.
+	 * アクセサ・オブジェクト──クエリの条件式で指定されたプロパティを要素から取得するためのオブジェクト──をパラメータとして受け取り、
+	 * ファクトリ・オブジェクトを初期化します。
+	 * @param accessor アクセサ・オブジェクト
+	 */
 	public QueryFactory(Accessor<E> accessor) {
 		if (accessor == null) {
 			throw new IllegalArgumentException();
 		}
 		this.a = accessor;
 	}
-	public Query<E> create(String expr) throws QueryParseException {
+	/**
+	 * 文字列として表現されたクエリをパースして解析済みクエリを生成する.
+	 * @param query クエリ文字列
+	 * @return 解析済みクエリ
+	 * @throws QueryParseException クエリ内容に問題があり解析に失敗した場合
+	 */
+	public Query<E> create(String query) throws QueryParseException {
 		try {
-			return new QueryImpl<E>(p.parse(expr), a);
+			return new QueryImpl<E>(p.parse(query), a);
 		} catch (ParseException e) {
 			throw new QueryParseException(e);
 		}
