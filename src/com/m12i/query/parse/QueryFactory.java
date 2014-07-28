d6e2fc8b3f4a7c738cf1840c5cefef91a210abed
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-07-28 21:41:40 +0900

    QueryFactoryのロジックにコメントを追加

--- src/com/m12i/query/parse/QueryFactory.java
+++ src/com/m12i/query/parse/QueryFactory.java
@@ -29,35 +29,50 @@ public final class QueryFactory<E> {
 	 * @return ファクトリ・オブジェクト
 	 */
 	public static<T> QueryFactory<T> createBeanQueryFactory(final Class<T> elemType) { 
+		// getterっぽいメソッドを格納しておくマップ
 		final HashMap<String, Method> methods = new HashMap<String, Method>();
 		try {
+			// 引数で指定されたクラスのpublicメソッドについて繰り返し処理
 			for (final Method m : elemType.getMethods()) {
+				// メソッドのシグネチャをチェック
 				if (m.getParameterTypes().length == 0 && m.getReturnType() != Void.class) {
+					// 「引数の数がゼロであり かつ 戻り値がvoidでない」場合はgetterとみなす
 					methods.put(m.getName(), m);
 				}
 			}
 		} catch(Exception e) {
 			// Do nothing.
 		}
+		// ファクトリを初期化して返す
 		return new QueryFactory<T>(new Accessor<T>() {
+			// getterをコールする際に使用するダミーの引数リスト
 			private final Object[] args = new Object[0];
+			// プロパティ名にマッチするgetterメソッドを返す
 			private Method getGetter(String prop) {
+				// JavaBeans規約に則ったgetter名を構成
 				final String getterName = "get" +
 						prop.substring(0, 1).toUpperCase() + prop.substring(1);
+				// getterっぽいメソッドを格納したマップの要素キーと照合
 				if (methods.containsKey(getterName)) {
+					// JavaBeans規約に則ったgetterが存在すればそれを返す
 					return methods.get(getterName);
 				} else if (methods.containsKey(prop)) {
+					// プロパティと同名のメソッドが存在すればそれを返す
 					return methods.get(prop);
 				} else {
+					// いずれにも該当しなければnullを返す
 					return null;
 				}
 			}
 			@Override
 			public String accsess(T elem, String prop) {
 				try {
+					// プロパティ名にマッチするメソッドを探す
 					final Method m = getGetter(prop);
+					// ダミーの引数リストに適用して結果を返す
 					return m == null ? null : m.invoke(elem, args).toString();
 				} catch (Exception e) {
+					// 何らかの理由でメソッドコールが失敗したらともかくnullを返す
 					return null;
 				}
 			}
