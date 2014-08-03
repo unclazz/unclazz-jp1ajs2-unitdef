39cc383ce22d80b5b2fb0bb6bf0d1bfbaae935f4
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-08-03 09:47:29 +0900

    EargerLoadParsableのテストケースを追加

--- test/test/com/m12i/code/parse/EargerLoadParsableTest.java
+++ test/test/com/m12i/code/parse/EargerLoadParsableTest.java
@@ -364,4 +364,129 @@ public class EargerLoadParsableTest {
 		assertThat(p.next(), is('\u0000'));
 	}
 
+
+	@Test
+	public void 空行から始まるファイルのパース() {
+		final Parsable p0 = newInstance("\r\n\r\n");
+		assertThat(p0.current(), is('\r'));
+		assertThat(p0.columnNo(), is(1));
+		assertThat(p0.lineNo(), is(1));
+		assertThat(p0.hasReachedEol(), is(true));
+		
+		assertThat(p0.next(), is('\n'));
+		assertThat(p0.columnNo(), is(2));
+		assertThat(p0.lineNo(), is(1));
+		assertThat(p0.hasReachedEol(), is(true));
+		
+		assertThat(p0.next(), is('\r'));
+		assertThat(p0.columnNo(), is(1));
+		assertThat(p0.lineNo(), is(2));
+		assertThat(p0.hasReachedEol(), is(true));
+
+		assertThat(p0.next(), is('\n'));
+		assertThat(p0.columnNo(), is(2));
+		assertThat(p0.lineNo(), is(2));
+		assertThat(p0.hasReachedEol(), is(true));
+
+		assertThat(p0.next(), is('\u0000'));
+		assertThat(p0.hasReachedEof(), is(true));
+
+		final Parsable p1 = newInstance("\r\n");
+		assertThat(p1.current(), is('\r'));
+		assertThat(p1.columnNo(), is(1));
+		assertThat(p1.lineNo(), is(1));
+		assertThat(p1.hasReachedEol(), is(true));
+
+		assertThat(p1.next(), is('\n'));
+		assertThat(p1.columnNo(), is(2));
+		assertThat(p1.lineNo(), is(1));
+		assertThat(p1.hasReachedEol(), is(true));
+
+		assertThat(p1.next(), is('\u0000'));
+		assertThat(p1.hasReachedEof(), is(true));
+
+		final Parsable p2 = newInstance("\r\nABC");
+		assertThat(p2.current(), is('\r'));
+		assertThat(p2.columnNo(), is(1));
+		assertThat(p2.lineNo(), is(1));
+		assertThat(p2.hasReachedEol(), is(true));
+
+		assertThat(p2.next(), is('\n'));
+		assertThat(p2.columnNo(), is(2));
+		assertThat(p2.lineNo(), is(1));
+		assertThat(p2.hasReachedEol(), is(true));
+
+		assertThat(p2.next(), is('A'));
+		assertThat(p2.columnNo(), is(1));
+		assertThat(p2.lineNo(), is(2));
+		assertThat(p2.hasReachedEol(), is(false));
+
+		assertThat(p2.next(), is('B'));
+		assertThat(p2.columnNo(), is(2));
+		assertThat(p2.lineNo(), is(2));
+		assertThat(p2.hasReachedEol(), is(false));
+
+		assertThat(p2.next(), is('C'));
+		assertThat(p2.columnNo(), is(3));
+		assertThat(p2.lineNo(), is(2));
+		assertThat(p2.hasReachedEol(), is(false));
+
+		assertThat(p2.next(), is('\u0000'));
+		assertThat(p2.hasReachedEof(), is(true));
+
+		final Parsable p3 = newInstance("\r\nABC\r\n");
+		assertThat(p3.current(), is('\r'));
+		assertThat(p3.columnNo(), is(1));
+		assertThat(p3.lineNo(), is(1));
+		assertThat(p3.hasReachedEol(), is(true));
+
+		assertThat(p3.next(), is('\n'));
+		assertThat(p3.columnNo(), is(2));
+		assertThat(p3.lineNo(), is(1));
+		assertThat(p3.hasReachedEol(), is(true));
+
+		assertThat(p3.next(), is('A'));
+		assertThat(p3.columnNo(), is(1));
+		assertThat(p3.lineNo(), is(2));
+		assertThat(p3.hasReachedEol(), is(false));
+
+		assertThat(p3.next(), is('B'));
+		assertThat(p3.columnNo(), is(2));
+		assertThat(p3.lineNo(), is(2));
+		assertThat(p3.hasReachedEol(), is(false));
+
+		assertThat(p3.next(), is('C'));
+		assertThat(p3.columnNo(), is(3));
+		assertThat(p3.lineNo(), is(2));
+		assertThat(p3.hasReachedEol(), is(false));
+		
+		assertThat(p3.next(), is('\r'));
+		assertThat(p3.columnNo(), is(4));
+		assertThat(p3.lineNo(), is(2));
+		assertThat(p3.hasReachedEol(), is(true));
+
+		assertThat(p3.next(), is('\n'));
+		assertThat(p3.columnNo(), is(5));
+		assertThat(p3.lineNo(), is(2));
+		assertThat(p3.hasReachedEol(), is(true));
+
+		assertThat(p3.next(), is('\u0000'));
+		assertThat(p3.hasReachedEof(), is(true));
+
+		final Parsable p4 = newInstance("\r\nABC\r\n\r\nDEF");
+		assertThat(p4.current(), is('\r'));
+		assertThat(p4.next(), is('\n'));
+		assertThat(p4.next(), is('A'));
+		assertThat(p4.next(), is('B'));
+		assertThat(p4.next(), is('C'));
+		assertThat(p4.next(), is('\r'));
+		assertThat(p4.next(), is('\n'));
+		assertThat(p4.next(), is('\r'));
+		assertThat(p4.next(), is('\n'));
+		assertThat(p4.next(), is('D'));
+		assertThat(p4.next(), is('E'));
+		assertThat(p4.next(), is('F'));
+		assertThat(p4.next(), is('\u0000'));
+		assertThat(p4.hasReachedEof(), is(true));
+	}
 }
