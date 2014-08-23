f378d5056d351cbaff528aaa4216dfb59ebaf00d
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-08-23 20:38:17 +0900

    Optionsの追加

--- src/com/m12i/code/parse/Parsers.java
+++ src/com/m12i/code/parse/Parsers.java
@@ -1,6 +1,58 @@
 package com.m12i.code.parse;
 
 public final class Parsers {
+	public static final class Options {
+		private String lineCommentStart;
+		private String blockCommentStart;
+		private String blockCommentEnd;
+		private char escapePrefixInDoubleQuotes;
+		private char escapePrefixInSingleQuotes;
+		private char escapePrefixInBackQuotes;
+		private boolean skipCommentWithWhitespace;
+		public String getLineCommentStart() {
+			return lineCommentStart;
+		}
+		public void setLineCommentStart(String lineCommentStart) {
+			this.lineCommentStart = lineCommentStart;
+		}
+		public String getBlockCommentStart() {
+			return blockCommentStart;
+		}
+		public void setBlockCommentStart(String blockCommentStart) {
+			this.blockCommentStart = blockCommentStart;
+		}
+		public String getBlockCommentEnd() {
+			return blockCommentEnd;
+		}
+		public void setBlockCommentEnd(String blockCommentEnd) {
+			this.blockCommentEnd = blockCommentEnd;
+		}
+		public char getEscapePrefixInDoubleQuotes() {
+			return escapePrefixInDoubleQuotes;
+		}
+		public void setEscapePrefixInDoubleQuotes(char escapePrefixInDoubleQuotes) {
+			this.escapePrefixInDoubleQuotes = escapePrefixInDoubleQuotes;
+		}
+		public char getEscapePrefixInSingleQuotes() {
+			return escapePrefixInSingleQuotes;
+		}
+		public void setEscapePrefixInSingleQuotes(char escapePrefixInSingleQuotes) {
+			this.escapePrefixInSingleQuotes = escapePrefixInSingleQuotes;
+		}
+		public char getEscapePrefixInBackQuotes() {
+			return escapePrefixInBackQuotes;
+		}
+		public void setEscapePrefixInBackQuotes(char escapePrefixInBackQuotes) {
+			this.escapePrefixInBackQuotes = escapePrefixInBackQuotes;
+		}
+		public boolean isSkipCommentWithWhitespace() {
+			return skipCommentWithWhitespace;
+		}
+		public void setSkipCommentWithWhitespace(boolean skipCommentWithWhitespace) {
+			this.skipCommentWithWhitespace = skipCommentWithWhitespace;
+		}
+	}
+	
 	
 	private final StringBuilder buff = new StringBuilder();
 	
