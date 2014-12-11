d108216d2ca0930f9db0f210ce7b90415037ee84
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-12-12 02:22:18 +0900

    UnitにJavadocを追加

--- src/main/java/com/m12i/jp1ajs2/unitdef/Unit.java
+++ src/main/java/com/m12i/jp1ajs2/unitdef/Unit.java
@@ -4,63 +4,78 @@ import java.util.List;
 
 import com.m12i.jp1ajs2.unitdef.util.Maybe;
 
+/**
+ * ユニット定義.
+ */
 public interface Unit extends Iterable<Unit> {
+	/**
+	 * ユニット名を返す.
+	 * @return ユニット名
+	 */
 	String getName();
+	/**
+	 * 許可モードを返す.
+	 * @return 許可モード
+	 */
 	Maybe<String> getPermissionMode();
+	/**
+	 * ユニット所有者となるJP1ユーザー名を返す.
+	 * @return JP1ユーザー名
+	 */
 	Maybe<String> getOwnerName();
+	/**
+	 * JP1資源グループ名を返す.
+	 * @return JP1資源グループ名
+	 */
 	Maybe<String> getResourceGroupName();
 
 	/**
 	 * ユニット定義パラメータのリストを返す. 
-	 * ユニット定義パラメータのなかには同名のエントリーの重複が許されるものがあります。
+	 * ユニット定義パラメータのなかには同名のエントリーの重複が許されるものがある。
 	 * @return ユニット定義パラメータのリスト
 	 */
 	List<Param> getParams();
 	/**
 	 * 引数で指定された名称のユニット定義パラメータのリストを返す.
-	 * 指定されたパラメータが存在しない場合は空のリストを返します。
 	 * @param paramName パラメータ名
 	 * @return ユニット定義パラメータのリスト
 	 */
 	List<Param> getParams(String paramName);
 	/**
-	 * サブユニットのユニット定義リストを返す.
-	 * 
+	 * 下位ユニットのユニット定義のリストを返す.
 	 * @return サブユニットのユニット定義リスト
 	 */
 	List<Unit> getSubUnits();
 
 	// すべてのユニットに（ほぼ）共通するパラメータにアクセスするためのユーティリティ
 	/**
-	 * 引数で指定された名前のサブユニットを返す.
-	 * 指定された名前のサブユニットが存在しない場合、{@code null}を返します。
-	 * 
-	 * @param targetUnitName 対象ユニット名
-	 * @return サブユニット
+	 * 引数で指定された名前の下位ユニットを返す.
+	 * @param unitName ユニット名
+	 * @return 下位ユニットを要素とする{@link Maybe}
 	 */
-	Maybe<Unit> getSubUnits(final String targetUnitName);
+	Maybe<Unit> getSubUnits(final String unitName);
 	
-	// TODO Javadoc
+	/**
+	 * 引数で指定された名前の子孫ユニットを返す.
+	 * @param unitName ユニット名
+	 * @return 子孫ユニットを要素とする{@link Maybe}
+	 */
 	Maybe<Unit> getDescendentUnits(final String unitName);
 	
 	/**
-	 * 属性定義情報"ty"で指定されたユニット種別を返す.
-	 * ユニット種別は必須パラメータであるため、このメソッドが{@code null}を返すことはありません。
-	 * 
+	 * 属性定義情報{@code "ty"}で指定されたユニット種別を返す.
+	 * ユニット種別は必須パラメータであるため、このメソッドが{@code null}を返すことはない。
 	 * @return ユニット種別
 	 */
 	UnitType getType();
 
 	/**
-	 * 属性定義情報"cm"で指定されたユニットのコメントを返す.
-	 * コメントは必須パラメータではありません。ユニットに"cm"が設定されていない場合、このメソッドは空文字列を返します。
-	 * 
+	 * 属性定義情報{@code "cm"}で指定されたユニットのコメントを返す.
 	 * @return コメント
 	 */
 	Maybe<String> getComment();
 	/**
 	 * ユニットの完全名を返す.
-	 * 
 	 * @return 完全名
 	 */
 	String getFullQualifiedName();
