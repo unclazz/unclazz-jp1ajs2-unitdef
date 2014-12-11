89854c53f580bf5c9e6d03af458eda22af38970c
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-12-12 01:37:46 +0900

    Unitsのパース系メソッドのシグネチャを修正／Javadocを書き直し

--- src/main/java/com/m12i/jp1ajs2/unitdef/Units.java
+++ src/main/java/com/m12i/jp1ajs2/unitdef/Units.java
@@ -1,67 +1,104 @@
 package com.m12i.jp1ajs2.unitdef;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+import com.m12i.jp1ajs2.unitdef.parser.ParseError;
 import com.m12i.jp1ajs2.unitdef.parser.UnitParser;
 import com.m12i.jp1ajs2.unitdef.parser.Input;
 import com.m12i.jp1ajs2.unitdef.util.Maybe;
-import com.m12i.jp1ajs2.unitdef.util.OneIterator;
-import com.m12i.jp1ajs2.unitdef.util.ZeroIterator;
 
-// TODO Javadoc, Test
+/**
+ * {@link Unit}のためのユーティリティを提供するオブジェクト.
+ */
 public final class Units {
-	private Units() {
+	private Units() {}
+
+	/**
+	 * ファイルからユニット定義情報を読み取る.
+	 * システム・デフォルトのキャラクターセットを使用する。
+	 * @param f ファイル
+	 * @return ユニット定義
+	 * @throws IOException I/Oエラーが発生した場合 
+	 * @throws IllegalArgumentException 構文エラーが検出された場合
+	 */
+	public static Unit fromFile(final File f) throws IOException {
+		return fromInput(Input.fromFile(f));
 	}
 
-	public static ParseResult fromFile(final File f) {
+	/**
+	 * ファイルからユニット定義情報を読み取る.
+	 * @param f ファイル
+	 * @param charset キャラクターセット
+	 * @return ユニット定義
+	 * @throws IOException I/Oエラーが発生した場合
+	 * @throws IllegalArgumentException 構文エラーが検出された場合
+	 */
+	public static Unit fromFile(final File f, final Charset charset) throws IOException {
+		return fromInput(Input.fromFile(f, charset));
+	}
+
+	/**
+	 * 入力ストリームからユニット定義情報を読み取る.
+	 * システム・デフォルトのキャラクターセットを使用する。
+	 * @param s ストリーム
+	 * @return ユニット定義
+	 * @throws IOException I/Oエラーが発生した場合
+	 * @throws IllegalArgumentException 構文エラーが検出された場合
+	 */
+	public static Unit fromStream(final InputStream s) throws IOException {
+		return fromInput(Input.fromStream(s));
+	}
+
+	/**
+	 * 入力ストリームからユニット定義情報を読み取る.
+	 * @param s ストリーム
+	 * @param charset キャラクターセット
+	 * @return ユニット定義
+	 * @throws IOException I/Oエラーが発生した場合
+	 * @throws IllegalArgumentException 構文エラーが検出された場合
+	 */
+	public static Unit fromStream(final InputStream s, final Charset charset) throws IOException {
+		return fromInput(Input.fromStream(s, charset));
+	}
+
+	/**
+	 * 文字列からユニット定義情報を読み取る.
+	 * @param s 文字列
+	 * @return ユニット定義
+	 * @throws IllegalArgumentException 構文エラーが検出された場合
+	 */
+	public static Unit fromString(final String s) {
 		try {
-			return fromInput(Input.fromFile(f));
-		} catch (Exception e) {
-			return ParseResult.failure(e);
+			return new UnitParser().parse(Input.fromString(s));
+		} catch (final ParseError e) {
+			throw new IllegalArgumentException(e);
 		}
-	}
-
-	public static ParseResult fromFile(final File f, final Charset charset) {
-		try {
-			return fromInput(Input.fromFile(f, charset));
-		} catch (Exception e) {
-			return ParseResult.failure(e);
-		}
-	}
-
-	public static ParseResult fromStream(final InputStream s) {
-		try {
-			return fromInput(Input.fromStream(s));
-		} catch (Exception e) {
-			return ParseResult.failure(e);
-		}
-	}
-
-	public static ParseResult fromStream(final InputStream s, final Charset charset) {
-		try {
-			return fromInput(Input.fromStream(s, charset));
-		} catch (Exception e) {
-			return ParseResult.failure(e);
-		}
-	}
-
-	public static ParseResult fromString(final String s) {
-		return fromInput(Input.fromString(s));
 	}
 	
-	private static ParseResult fromInput(final Input in) {
+	private static Unit fromInput(final Input in) throws IOException {
 		try {
-			return ParseResult.success(new UnitParser().parse(in));
-		} catch (Exception e) {
-			return ParseResult.failure(e);
+			return new UnitParser().parse(in);
+		} catch (final ParseError e) {
+			final Throwable cause = e.getCause();
+			if (cause != null && cause instanceof IOException) {
+				throw (IOException)cause;
+			} else {
+				throw new IllegalArgumentException(e);
+			}
 		}
 	}
 	
+	/**
+	 * 引数に指定されたユニット定義とその子孫のユニット定義を要素とするリストを返す.
+	 * リスト要素は深さ優先探索により発見された順序で並ぶ。
+	 * @param unit ユニット
+	 * @return ユニット定義リスト
+	 */
 	public static List<Unit> asList(Unit unit) {
 		final ArrayList<Unit> list = new ArrayList<>();
 		collectSubUnits(list, unit);
@@ -75,6 +112,12 @@ public final class Units {
 		}
 	}
 	
+	/**
+	 * ユニット定義パラメータを検索して返す.
+	 * @param unit ユニット定義
+	 * @param paramName ユニット定義パラメータ名
+	 * @return ユニット定義パラメータを要素とする{@link Maybe}
+	 */
 	public static Maybe<Param> getParams(final Unit unit, final String paramName) {
 		final List<Param> list = new ArrayList<Param>();
 		for (final Param p : unit.getParams()) {
@@ -84,7 +127,13 @@ public final class Units {
 		}
 		return Maybe.wrap(list);
 	}
-	
+
+	/**
+	 * 子にあたるユニット定義を検索して返す.
+	 * @param unit ユニット定義 
+	 * @param unitName ユニット名
+	 * @return ユニット定義を要素とする{@link Maybe}
+	 */
 	public static Maybe<Unit> getSubUnits(final Unit unit, final String unitName) {
 		final List<Unit> list = new ArrayList<Unit>();
 		for (final Unit u : unit.getSubUnits()) {
@@ -95,6 +144,12 @@ public final class Units {
 		return Maybe.wrap(list);
 	}
 	
+	/**
+	 * 子孫のユニット定義を検索して返す.
+	 * @param unit ユニット定義
+	 * @param unitName ユニット名
+	 * @return ユニット定義を要素とする{@link Maybe}
+	 */
 	public static Maybe<Unit> getDescendentUnits(final Unit unit, final String unitName) {
 		final List<Unit> list = new ArrayList<Unit>();
 		for (final Unit u : asList(unit)	) {
@@ -103,69 +158,5 @@ public final class Units {
 			}
 		}
 		return Maybe.wrap(list);
-	}
-	
-	public static final class ParseResult implements Iterable<Unit> {
-		public static ParseResult success(Unit r) {
-			return new ParseResult(null, r);
-		}
-		public static ParseResult failure(String message) {
-			return new ParseResult(new RuntimeException(message), null);
-		}
-		public static ParseResult failure(Throwable error) {
-			return new ParseResult(error, null);
-		}
-		
-		private final Throwable l;
-		private final Unit r;
-		public ParseResult (Throwable error, Unit unit) {
-			this.l = error;
-			this.r = unit;
-		}
-		public boolean isFailure() {
-			return l != null;
-		}
-		public boolean isSuccess() {
-			return l == null;
-		}
-		public Throwable error() {
-			if (isFailure()) {
-				return l;
-			}
-			throw new RuntimeException();
-		}
-		public Unit unit() {
-			if (isSuccess()) {
-				return r;
-			}
-			throw new RuntimeException();
-		}
-		@Override
-		public Iterator<Unit> iterator() {
-			if (isFailure()) {
-				return ZeroIterator.getInstance();
-			} else {
-				return new OneIterator<Unit>(r);
-			}
-		}
-		@Override
-		public String toString() {
-			return (isFailure() ? "Failure(" : "Success(") + error() + ")";
-		}
-		@Override
-		public boolean equals(Object other) {
-			if (other == null || other.getClass() != ParseResult.class) {
-				return false;
-			}
-			final ParseResult that = (ParseResult) other;
-			if (this.isFailure() != that.isSuccess()) {
-				return false;
-			}
-			return this.isFailure() ? (this.error().equals(that.error())) : (this.unit().equals(that.unit()));
-		}
-		@Override
-		public int hashCode() {
-			return 37 * (this.isFailure() ? this.error() : this.unit()).hashCode();
-		}
 	}
 }
