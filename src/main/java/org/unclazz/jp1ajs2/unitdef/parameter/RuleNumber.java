f2a32104e0be47cebcf61c1631ab48c068bc61ee
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-02-07 19:30:03 +0900

    RuleNumberのJavadocを追加

--- src/main/java/org/unclazz/jp1ajs2/unitdef/parameter/RuleNumber.java
+++ src/main/java/org/unclazz/jp1ajs2/unitdef/parameter/RuleNumber.java
@@ -1,11 +1,27 @@
 package org.unclazz.jp1ajs2.unitdef.parameter;
 
+/**
+ * 上位ジョブネットのスケジュール・ルール番号を表わすオブジェクト.
+ * 指定できる数値は{@code 0}から{@code 144}の範囲であるが、
+ * {@code 0}は特殊な意味を持っておりsd=0,udのかたちでしか指定できない。
+ */
 public final class RuleNumber implements Comparable<RuleNumber> {
+	/** 実行開始日が未定義（sd=0,ud）のときのみ使用される値. */
 	public static final RuleNumber UNDEFINED = new RuleNumber(0);
+	/** 最小値. {@code 1}を表わす。上述のとおり{@code 0}は特殊な意味を持っており
+	 * ふつう利用できる値の最小値は{@code 1}である。 */
 	public static final RuleNumber MIN = new RuleNumber(1);
+	/** 最大値. {@code 144}を表わす。 */
 	public static final RuleNumber MAX = new RuleNumber(144);
+	/** デフォルト値. {@link #MIN}と同じ。 */
 	public static final RuleNumber DEFAULT = MIN;
 	
+	/**
+	 * 指定されたルール番号に対応するインスタンスを返す.
+	 * @param n ルール番号
+	 * @return インスタンス
+	 * @throws IllegalArgumentException 指定された値が表現可能な範囲を超えていた場合
+	 */
 	public static RuleNumber of(final int n) {
 		switch (n) {
 		case 0:
