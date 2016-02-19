93eaf5304ea90a211ec7ce4bbe072793993d6c58
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-02-20 00:23:05 +0900

    BuildersにJavadocを追加

--- src/main/java/org/unclazz/jp1ajs2/unitdef/builder/Builders.java
+++ src/main/java/org/unclazz/jp1ajs2/unitdef/builder/Builders.java
@@ -1,54 +1,134 @@
 package org.unclazz.jp1ajs2.unitdef.builder;
 
+import org.unclazz.jp1ajs2.unitdef.Attributes;
+import org.unclazz.jp1ajs2.unitdef.FullQualifiedName;
+import org.unclazz.jp1ajs2.unitdef.Parameter;
 import org.unclazz.jp1ajs2.unitdef.ParameterValue;
 import org.unclazz.jp1ajs2.unitdef.PermissionMode;
 import org.unclazz.jp1ajs2.unitdef.Tuple;
+import org.unclazz.jp1ajs2.unitdef.Unit;
+import org.unclazz.jp1ajs2.unitdef.parameter.AnteroposteriorRelationship;
+import org.unclazz.jp1ajs2.unitdef.parameter.Element;
+import org.unclazz.jp1ajs2.unitdef.parameter.EndDelayTime;
+import org.unclazz.jp1ajs2.unitdef.parameter.StartDate;
+import org.unclazz.jp1ajs2.unitdef.parameter.StartDelayTime;
+import org.unclazz.jp1ajs2.unitdef.parameter.StartTime;
 
+/**
+ * ユニット定義情報を構成するコンポーネントのためのビルダーを提供するユーティリティ.
+ */
 public final class Builders {
 	private Builders() {}
 	
+	/**
+	 * {@link Tuple}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static TupleBuilder tuple() {
 		return new TupleBuilder();
 	}
+	/**
+	 * {@link Parameter}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static ParameterBuilder parameter() {
 		return new ParameterBuilder();
 	}
+	/**
+	 * {@link Unit}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static UnitBuilder unit() {
 		return new UnitBuilder();
 	}
+	/**
+	 * {@link Attributes}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static AttributesBuilder attributes() {
 		return new AttributesBuilder();
 	}
+	/**
+	 * {@link FullQualifiedName}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static FullQualifiedNameBuilder fullQualifiedName() {
 		return new FullQualifiedNameBuilder();
 	}
+	/**
+	 * {@link AnteroposteriorRelationship}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static AnteroposteriorRelationshipBuilder parameterAR() {
 		return new AnteroposteriorRelationshipBuilder();
 	}
-	public static EndScheduledTimeBuilder parameterEY() {
-		return new EndScheduledTimeBuilder();
+	/**
+	 * {@link EndDelayTime}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
+	public static EndDelayTimeBuilder parameterEY() {
+		return new EndDelayTimeBuilder();
 	}
-	public static StartScheduledTimeBuilder parameterSY() {
-		return new StartScheduledTimeBuilder();
+	/**
+	 * {@link StartDelayTime}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
+	public static StartDelayTimeBuilder parameterSY() {
+		return new StartDelayTimeBuilder();
 	}
+	/**
+	 * {@link StartTime}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static StartTimeBuilder parameterST() {
 		return new StartTimeBuilder();
 	}
+	/**
+	 * {@link StartDate}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static StartDateBuilder parameterSD() {
 		return new StartDateBuilder();
 	}
+	/**
+	 * {@link Element}のためのビルダーを返す.
+	 * @return ビルダー
+	 */
 	public static ElementBuilder parameterEL() {
 		return new ElementBuilder();
 	}
+	/**
+	 * 文字シーケンスを内容とする{@link ParameterValue}のインスタンスを返す.
+	 * @param value ユニット定義パラメータ値の内容
+	 * @return ユニット定義パラメータ値
+	 */
 	public static ParameterValue charSequenceParameterValue(final CharSequence value) {
 		return new CharSequenceParameterValue(value);
 	}
+	/**
+	 * 引用符で囲われた文字シーケンスを内容とする{@link ParameterValue}のインスタンスを返す.
+	 * @param value ユニット定義パラメータ値の内容
+	 * @return ユニット定義パラメータ値
+	 */
 	public static ParameterValue quotedParameterValue(final CharSequence value) {
 		return new QuotedParameterValue(value);
 	}
+	/**
+	 * タプルもどきを内容とする{@link ParameterValue}のインスタンスを返す.
+	 * @param value ユニット定義パラメータ値の内容
+	 * @return ユニット定義パラメータ値
+	 */
 	public static ParameterValue tupleParameterValue(final Tuple value) {
 		return new TupleParameterValue(value);
 	}
+	/**
+	 * {@link PermissionMode}のインスタンスを返す.
+	 * <p>空文字列もしくは{@code null}が引数に指定された場合、パラメータの指定が省略されていることを示すインスタンスが返される。
+	 * 4桁の8進数文字列が指定された場合、その値を内容とするインスタンスが返される。
+	 * それ以外の値が指定された場合、このメソッドは実行時例外をスローする。</p>
+	 * @param hex4 4桁の8進数
+	 * @return 許可モード
+	 */
 	public static PermissionMode permissionMode(final CharSequence hex4) {
 		return hex4 == null || hex4.length() == 0 
 				? DefaultPermissionMode.NOT_SPECIFIED 
--- src/main/java/org/unclazz/jp1ajs2/unitdef/builder/EndDelayTimeBuilder.java
+++ src/main/java/org/unclazz/jp1ajs2/unitdef/builder/EndDelayTimeBuilder.java
@@ -5,26 +5,26 @@ import org.unclazz.jp1ajs2.unitdef.parameter.RuleNumber;
 import org.unclazz.jp1ajs2.unitdef.parameter.Time;
 import org.unclazz.jp1ajs2.unitdef.parameter.DelayTime.TimingMethod;
 
-public class EndScheduledTimeBuilder {
-	EndScheduledTimeBuilder() {}
+public class EndDelayTimeBuilder {
+	EndDelayTimeBuilder() {}
 	
 	private TimingMethod timingMethod = null;
 	private Time time = null;
 	private RuleNumber ruleNumber = RuleNumber.DEFAULT;
 	
-	public EndScheduledTimeBuilder setTimingMethod(TimingMethod timingMethod) {
+	public EndDelayTimeBuilder setTimingMethod(TimingMethod timingMethod) {
 		this.timingMethod = timingMethod;
 		return this;
 	}
-	public EndScheduledTimeBuilder setRuleNumber(RuleNumber ruleNumber) {
+	public EndDelayTimeBuilder setRuleNumber(RuleNumber ruleNumber) {
 		this.ruleNumber = ruleNumber;
 		return this;
 	}
-	public EndScheduledTimeBuilder setRuleNumber(int ruleNumber) {
+	public EndDelayTimeBuilder setRuleNumber(int ruleNumber) {
 		this.ruleNumber = RuleNumber.of(ruleNumber);
 		return this;
 	}
-	public EndScheduledTimeBuilder setTime(Time time) {
+	public EndDelayTimeBuilder setTime(Time time) {
 		this.time = time;
 		return this;
 	}
