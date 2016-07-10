034bc841ac763f72ac284e43235403b9c8393d80
mizuky fujitani <mizuky fujitani>
2016-07-10 21:13:26 +0900

    IterableQuerySupportにアノテーションを追加

--- src/main/java/org/unclazz/jp1ajs2/unitdef/query/IterableQuerySupport.java
+++ src/main/java/org/unclazz/jp1ajs2/unitdef/query/IterableQuerySupport.java
@@ -4,24 +4,31 @@ import org.unclazz.jp1ajs2.unitdef.util.Predicate;
 
 abstract class IterableQuerySupport<T,U> implements IterableQuery<T,U> {
 	public abstract Query<T,Iterable<U>> and(final Predicate<U> pred);
+	@Override
 	public final OneQuery<T, U> one(U defaultValue) {
 		return new OneQuery<T, U>(this, defaultValue);
 	}
+	@Override
 	public final OneQuery<T, U> one(boolean nullable) {
 		return new OneQuery<T, U>(this, nullable);
 	}
+	@Override
 	public final OneQuery<T, U> one() {
 		return new OneQuery<T, U>(this, false);
 	}
+	@Override
 	public final ListQuery<T, U> list() {
 		return new StrictListQuery<T, U>(this, false);
 	}
+	@Override
 	public final ListQuery<T, U> list(boolean cached) {
 		return new StrictListQuery<T, U>(this, cached);
 	}
+	@Override
 	public final Query<T,Iterable<U>> cached() {
 		return CachedQuery.wrap(this);
 	}
+	@Override
 	public final<V> TypedValueIterableQuery<T, U, V> query(final Query<U, V> q) {
 		return new TypedValueIterableQuery<T, U, V>(this, q);
 	}
