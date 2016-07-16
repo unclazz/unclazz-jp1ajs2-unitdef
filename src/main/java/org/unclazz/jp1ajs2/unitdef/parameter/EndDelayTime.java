4325bca2e3f3f7b8db8f917e3202a837a31d5c88
mizuky fujitani <mizuky fujitani>
2016-07-16 14:15:01 +0900

    バージョン2.6.0のJavadocを追加

--- src/main/java/org/unclazz/jp1ajs2/unitdef/parameter/EndDelayTime.java
+++
@@ -1,6 +0,0 @@
-package org.unclazz.jp1ajs2.unitdef.parameter;
-
-/**
- * ユニット定義パラメータey（終了遅延時刻）を表わすオブジェクト.
- */
-public interface EndDelayTime extends DelayTime {}
--- src/main/java/org/unclazz/jp1ajs2/unitdef/parameter/StartDelayTime.java
+++
@@ -1,6 +0,0 @@
-package org.unclazz.jp1ajs2.unitdef.parameter;
-
-/**
- * ユニット定義パラメータsy（開始遅延時刻）を表わすオブジェクト.
- */
-public interface StartDelayTime extends DelayTime {}
